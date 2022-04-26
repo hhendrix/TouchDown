22608347bedde4f0534b5c93d0ad059a3367c1c0
Harry Lopez <harry.lopez@overactive.com>
2022-04-26 17:00:24 -0500

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Harry Lopez on 26/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
